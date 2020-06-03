937971d7d2dfb953bea14f722e6f6b954931d699
hudson dias <hudsondias@MacBook-Air-de-hudson.local>
2020-06-03 13:41:09 -0300

    Request

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
-import express, { response } from 'express';
+import express from 'express';
+import { Request,Response } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
 
 routes.get('/items',async (request,response) => {
+    console.log(request.body);
     const items =  await knex('items').select('*');
 
     const serializedItems = items.map(item => {
         return {
+            id:item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`
         }
@@ -16,8 +19,8 @@ routes.get('/items',async (request,response) => {
     return response.json(serializedItems)
 });
 
-routes.post('/points', async (request,response) =>{
-    console.log(request.body);
+routes.post('/points', async (request:Request,response:Response) =>{
+    
     const {
         name,
         email,
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import routes from './routes';
 
 let app = express();
-
+app.use(express.json());
 app.use(routes);
 app.use('/upload', express.static(path.resolve(__dirname, '..', 'upload')));
 app.listen(3333);
